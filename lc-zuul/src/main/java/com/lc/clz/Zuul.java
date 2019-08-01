39c76a5c63332b6628423dc06b9835f65ff198bc
chenglezheng <981797842@qq.com>
2019-08-01 10:39:22 +0800

    zuul中能通过postman正常访问请求了，但是swagger访问接口时仍出现报错

--- lc-zuul/src/main/java/com/lc/clz/Zuul.java
+++ lc-zuul/src/main/java/com/lc/clz/Zuul.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
-@EnableOAuth2Sso
 public class Zuul {
 
     public static void main(String[] args) {
