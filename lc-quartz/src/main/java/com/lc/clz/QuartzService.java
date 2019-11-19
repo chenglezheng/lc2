9dfeec24d1f5d4db0beed27f3181efe8c05448a6
chenglezheng <981797842@qq.com>
2019-11-19 20:00:02 +0800

    调试Quartz服务

--- lc-quartz/src/main/java/com/lc/clz/QuartzService.java
+++ lc-quartz/src/main/java/com/lc/clz/QuartzService.java
@@ -2,8 +2,10 @@ package com.lc.clz;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient  //服务客户端，让该服务注册到Eureka中
 public class QuartzService {
     public static void main(String[] args){
         new SpringApplicationBuilder(QuartzService.class).run(args);
